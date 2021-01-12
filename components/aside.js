e915e6f7f8aa6408abe4340c8461e5ce2e8327bc
Kelly Harrop <kelly_harrop@intuit.com>
2021-01-12 00:55:11 -0600

    adjusting dark colors

--- components/aside.js
+++ components/aside.js
@@ -35,17 +35,19 @@ export default function Aside() {
           </a>
         </li>
       </ul>
-      <dl className="w-48 text-sm text-gray-600">
+      <dl className="w-48 text-sm text-gray-600 dark:text-gray-500">
         <dt>
-          <h4>Currently</h4>
+          <h4 className="dark:text-gray-600">Currently</h4>
         </dt>
         <dd>Intuit Design System</dd>
         <dt>
-          <h4>Location</h4>
+          <h4 className="dark:text-gray-600">Location</h4>
         </dt>
         <dd>Plano, TX (remote)</dd>
       </dl>
-      <p className="text-xs">Written, designed, and coded by Kelly Harrop.</p>
+      <p className="text-xs text-gray-600 dark:text-gray-500">
+        Designed, written and coded by Kelly Harrop.
+      </p>
     </div>
   )
 }
